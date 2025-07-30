e4b76dd4bd5ba34d972dcb2edb6f3d0cd250ef8c
Ali hrera <alihrhera1@gmail.com>
2025-07-30 12:04:48 +0300

    Create HomeScreen with RestaurantMenu Composable
    This commit introduces the `HomeScreen` composable, which displays a restaurant menu.
    
    The `HomeScreen` fetches data using `HomeScreenViewModel` and displays it through the `RestaurantMenu` composable.
    
    The `RestaurantMenu` composable includes:
    - `AppTopAppBar` for the application's top bar.
    - `CategoryTabs` for selecting menu categories.
    - `SortOptionsRow` for sorting menu items based on price, rating, availability, popularity (hot), and vegan options.
    - A display area for menu items using `MenuItemsCards`.
    - Indicators for offline status and error messages.
    - Functionality for pull-to-refresh and marking items as favorites.
    
    A preview function, `RestaurantMenuAppPreview`, is also included for design-time visualization.

--- app/src/main/java/com/example/hilalplaytest/ui/MainActivity.kt
+++ app/src/main/java/com/example/hilalplaytest/ui/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.hilalplaytest.ui.home.HomeScreen
 import com.example.hilalplaytest.ui.theme.HilalPlayTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HilalPlayTestTheme {
                 Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
-
+                    HomeScreen()
                 }
             }
         }

103b9573a5e2e22cb3d79dcbfc9534a5b0538fb6
Ali hrera <alihrhera1@gmail.com>
2025-07-30 11:29:17 +0300

    Add ApiService with getMenu function

--- app/src/main/java/com/example/hilalplaytest/data/remot/api/ApiService.kt
+++ app/src/main/java/com/example/hilalplaytest/data/remot/api/ApiService.kt
@@ -1,7 +1,13 @@
 package com.example.hilalplaytest.data.remot.api
 
+import com.example.hilalplaytest.data.remot.dto.JsonbinResponseDto
+import com.example.hilalplaytest.data.remot.network.GET_MENU
+import retrofit2.http.GET
+
 
 interface ApiService {
 
+    @GET(GET_MENU)
+    suspend fun  getMenu(): JsonbinResponseDto
 
 }
